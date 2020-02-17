dd2c3cb7286eb7a8bb51db5eff7a49185d63e05c
Benjamin Dendas <benjamin.dendas@craftworkz.be>
2020-02-17 10:03:33 +0100

    KAS-385: toevoegen vlaams parlement service in dispatcher

--- config/dispatcher/dispatcher.ex
+++ config/dispatcher/dispatcher.ex
@@ -358,6 +358,10 @@ defmodule Dispatcher do
     Proxy.forward conn, path, "http://cache/mail-campaigns/"
   end
 
+  match "/vlaams-parlement-service/*path", @any do
+    Proxy.forward conn, path, "http://vlaams-parlement-service/"
+  end
+
   match "_", %{ last_call: true } do
     send_resp( conn, 404, "Route not found.  See config/dispatcher.ex" )
   end
